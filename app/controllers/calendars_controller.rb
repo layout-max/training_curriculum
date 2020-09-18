2be82714971ab2fe73576e4a326669021c26815e
layout-max <70515554+layout-max@users.noreply.github.com>
2020-09-18 15:33:35 +0900

    修正１

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,13 +1,13 @@
 class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
-  def get_index
+  def index
     getWeek
     @plan = Plan.new
   end
 
   # 予定の保存
-  def plan_create
+  def plan
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -25,7 +25,7 @@ class CalendarsController < ApplicationController
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
-    @get_week_days = []
+    @week_days = []
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
